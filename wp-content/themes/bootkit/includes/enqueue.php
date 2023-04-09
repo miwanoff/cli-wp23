1a4e64ad66bab7cac0b1659a568b8687f6b4a142
miwanoff <marina.iva.noffa@gmail.com>
2023-04-09 15:47:19 +0300

    Add scripts bootstrap jquery

--- wp-content/themes/bootkit/includes/enqueue.php
+++ wp-content/themes/bootkit/includes/enqueue.php
@@ -1,10 +1,9 @@
 <?php
 
-
 function bootkit_enqueue()
 {
     //echo 'Hello';
-   $url = get_theme_file_uri();
+    $url = get_theme_file_uri();
     // echo $url . '/assets/vendor/bootstrap/bootstrap.min.css';
     wp_register_style('bootkit_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); // Google fonts
     wp_register_style('bootkit_bootstrap', $url . '/assets/vendor/bootstrap/css/bootstrap.min.css'); // bootstrap.min.css
@@ -12,4 +11,8 @@ function bootkit_enqueue()
     wp_enqueue_style('bootkit_google_fonts');
     wp_enqueue_style('bootkit_bootstrap');
     wp_enqueue_style('bootkit_modern_business');
+    wp_register_script('bootkit_bootstrap', $url .
+        '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', 'jquery', false, true);
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootkit_bootstrap');
 }
