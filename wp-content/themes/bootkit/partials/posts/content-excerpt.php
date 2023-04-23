ed49c61ccaf462b1e9f676e2a9f2fea16f727e7f
miwanoff <marina.iva.noffa@gmail.com>
2023-04-23 15:26:48 +0300

    Add permalonks and date

--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -1,15 +1,19 @@
             <!-- Blog Post -->
             <div class="card mb-4">
-                <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
+                <?php if (has_post_thumbnail()) {
+    the_post_thumbnail("full", ["class" => "card-img-top"]);
+}?>
                 <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
+                    <!-- <h2 class="card-title">Post Title</h2> -->
+                    <h2 class="card-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
                         atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
                         voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
+                    <a href="<?php the_permalink()?>" class="btn btn-primary">Read More &rarr;</a>
                 </div>
                 <div class="card-footer text-muted">
-                    Posted on January 1, 2017 by
+                    Posted on <?php echo get_the_date() ?> by
                     <a href="#">Start Bootstrap</a>
                 </div>
             </div>
