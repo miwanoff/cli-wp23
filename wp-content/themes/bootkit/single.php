5f82562699463b9252c7719458aa8ec0855db033
miwanoff <marina.iva.noffa@gmail.com>
2023-04-29 14:59:56 +0300

    Add Author and Date/Time

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -8,12 +8,32 @@
             <?php if (have_posts()) {
     while (have_posts()) {
         the_post();
+        global $post;
+        $author_ID = $post->post_author;
+        $author_URL = get_author_posts_url($author_ID);
         ?>
             <!-- Title -->
             <h1 class="mt-4"><?php the_title()?></h1>
-
+            <!-- Author -->
+            <p class="lead">
+                by
+                <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+            </p>
             <!-- Post category: -->
-            <h2 class="mt-4"><?php the_category(" ")?></h2>
+            <h3 class="mt-4"><?php the_category(" ")?></h3>
+            <?php
+if (has_post_thumbnail()) {
+            the_post_thumbnail("full", ["class" => "card-img-top"]);
+        }
+        ?>
+
+            <!-- Date/Time -->
+            <p><?php the_time(get_option('date_format'));
+        echo " ";
+        the_time(get_option('time_format'));?></p>
+            <hr>
+
+            <hr>
             <p class="card-text">
                 <!-- Post Content -->
                 <?php the_content();?>
