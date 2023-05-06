82d16d0ed3b696197f33f63ec63ef74d7d2f637f
miwanoff <marina.iva.noffa@gmail.com>
2023-05-06 15:19:40 +0300

    Add time_to_post_content plugin

--- wp-content/plugins/hook-example.php
+++ wp-content/plugins/hook-example.php
@@ -9,3 +9,22 @@
 }
 
 add_filter('the_title', 'hooked_title');
+
+function added_footer()
+{
+    echo 'Added to footer by hook-example plugin ';
+}
+add_action('wp_footer', 'added_footer');
+
+function my_footer()
+{
+    do_action('wp_hook_footer');
+}
+add_action('wp_footer', 'my_footer');
+
+
+function hello_footer()
+{
+    echo 'Hello ';
+}
+add_action('wp_hook_footer', 'hello_footer');
--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -26,27 +26,3 @@ add_action('customize_register', 'bootkit_customize_register');
 // Shortcodes
 
 // Tests
-// function hooked_title($title)
-// {
-//     return 'Hooked ' . $title;
-// }
-// add_filter('the_title', 'hooked_title');
-
-function added_footer()
-{
-    echo 'Added to footer by hook-example plugin ';
-}
-add_action('wp_footer', 'added_footer');
-
-function my_footer()
-{
-    do_action('wp_hook_footer');
-}
-add_action('wp_footer', 'my_footer');
-
-
-function hello_footer()
-{
-    echo 'Hello ';
-}
-add_action('wp_hook_footer', 'hello_footer');
