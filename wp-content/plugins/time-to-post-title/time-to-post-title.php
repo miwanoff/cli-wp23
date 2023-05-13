fc1b033daf215669784ee4e0e605fe612557f80d
miwanoff <marina.iva.noffa@gmail.com>
2023-05-13 15:04:13 +0300

    Add shortcode to time-to-post-title plugin

--- wp-content/plugins/time-to-post-title/time-to-post-title.php
+++ wp-content/plugins/time-to-post-title/time-to-post-title.php
@@ -15,8 +15,16 @@ function time_to_post_content($content)
 }
 add_filter('the_content', 'time_to_post_content');
 
-add_action('plugins_loaded', 'time_load_plugin_textdomain');
+
+
 function time_load_plugin_textdomain()
 {
     load_plugin_textdomain('time-to-post-title', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
+add_action('plugins_loaded', 'time_load_plugin_textdomain');
+
+function time_shortcode($atts)
+{
+    return time_to_post_content($content);
+}
+add_shortcode('time_to_post', 'time_shortcode');
