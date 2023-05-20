69876aaade8ab9bceb677440b190ae3fd73173e6
miwanoff <marina.iva.noffa@gmail.com>
2023-05-20 14:59:13 +0300

    Add site name and description

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -61,14 +61,25 @@ class bootkit_widget extends WP_Widget
         );
     }
 
+    // public function widget($args, $instance)
+    // {
+    //     $title = apply_filters('widget_title', $instance['title']);
+    //     echo $args['before_widget'];
+    //     if (!empty($title)) {
+    //         echo $args['before_title'] . $title . $args['after_title'];
+    //     }
+    //     echo __('Hello, bootkit', 'bootkit_widget_domain');
+    // }
+
     public function widget($args, $instance)
     {
         $title = apply_filters('widget_title', $instance['title']);
-        echo $args['before_widget'];
-        if (!empty($title)) {
-            echo $args['before_title'] . $title . $args['after_title'];
-        }
-        echo __('Hello, bootkit', 'bootkit_widget_domain');
+        $blog_title = get_bloginfo('name');
+        $tagline = get_bloginfo('description');
+        echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];?>
+<p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
+<p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+<?php echo $args['after_widget'];
     }
 
     public function form($instance)
