0989288c936cb6634ef0a4fbe3954dd9f439f02d
miwanoff <marina.iva.noffa@gmail.com>
2023-05-21 15:28:25 +0300

    Add WP_Query

--- wp-content/plugins/kit-carousel/process/kc_show_carousel.php
+++ wp-content/plugins/kit-carousel/process/kc_show_carousel.php
@@ -1,50 +1,82 @@
 <?php
 function kc_show_carousel($content)
 {
-    $html =
-        '<section id="demos">
-        <div class="row">
-            <div class="large-12 columns">
-                <div class="owl-carousel owl-theme">
-                    <div class="item">
-                        <h4>1</h4>
-                    </div>
-                    <div class="item">
-                        <h4>2</h4>
-                    </div>
-                    <div class="item">
-                        <h4>3</h4>
-                    </div>
-                    <div class="item">
-                        <h4>4</h4>
-                    </div>
-                    <div class="item">
-                        <h4>5</h4>
-                    </div>
-                    <div class="item">
-                        <h4>6</h4>
-                    </div>
-                    <div class="item">
-                        <h4>7</h4>
-                    </div>
-                    <div class="item">
-                        <h4>8</h4>
-                    </div>
-                    <div class="item">
-                        <h4>9</h4>
-                    </div>
-                    <div class="item">
-                        <h4>10</h4>
-                    </div>
-                    <div class="item">
-                        <h4>11</h4>
-                    </div>
-                    <div class="item">
-                        <h4>12</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>';
+    $args = [
+        'post_type' => 'post',
+        'showposts' => 6,
+        'category_name' => 'news',
+
+        //'tag' => 'news',
+        'post_status' => 'publish',
+        'orderby' => 'date',
+        'order' => 'DESC',
+    ];
+
+    $query = new WP_Query($args);
+
+    $html = "";
+
+    if ($query->have_posts()) {
+        $html = '<section id="demos">
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="owl-carousel owl-theme">';
+        while ($query->have_posts()) {
+            $query->the_post();
+            $html .= '<div class="item" style="background:grey url(' . get_the_post_thumbnail_url($query->post->ID, 'thumbnail') . '); background-size:cover;"><h5>';
+            $html .= '<a href="' . get_permalink($query->post->ID) . '" style="color: white;text-decoration: none;">' . $query->post->post_title . '</a>';
+            $html .= '</h5></div>';
+        }
+        $html .= ' </div>
+    </div>
+</div>
+</section>';
+    }
+
+    // $html =
+    //     '<section id="demos">
+    //     <div class="row">
+    //         <div class="large-12 columns">
+    //             <div class="owl-carousel owl-theme">
+    //                 <div class="item">
+    //                     <h4>1</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>2</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>3</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>4</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>5</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>6</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>7</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>8</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>9</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>10</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>11</h4>
+    //                 </div>
+    //                 <div class="item">
+    //                     <h4>12</h4>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </section>';
     return $content . $html;
 }
